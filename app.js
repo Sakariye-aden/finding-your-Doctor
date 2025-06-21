1c31a3b7a974440dd74fc3cbeba234682848615e
Sakariye-aden <sakariyeadenhaji@gmail.com>
2025-06-21 09:31:38 +0300

    added dark/light mode task

--- app.js
+++ app.js
@@ -7,7 +7,25 @@ menu.addEventListener('click', function (){
     menu.classList.toggle('active')
 })
 
+
  function modeIcon(){
     i[0].toggleAttribute('data' );
     i[1].toggleAttribute( "data");
+
+     document.body.classList.toggle('active');
+//   saving dark into local storage
+    if(document.body.classList.contains('active')){
+        localStorage.setItem('mode','Dark')  
+    }else{
+         localStorage.setItem('mode','Light')
+    }
+}
+// loadding dark /light in to localstorage
+window.addEventListener('DOMContentLoaded',turnOn)
+function turnOn(){
+     // checking the local stroge
+     const savemode = localStorage.getItem('mode')
+    if(savemode === "Dark"){
+        document.body.classList.add('active')
+    }
 }
