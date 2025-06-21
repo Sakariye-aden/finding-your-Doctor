f178a395f96f60dccbb3dea490ad6d6fb90fce1c
Sakariye-aden <sakariyeadenhaji@gmail.com>
2025-06-21 08:48:24 +0300

    added navigation

---
+++ app.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('.menu');
+const navigation = document.querySelector('.navigation');
+const mode = document.querySelector('#mode');
+const  i = document.querySelectorAll('i');
+
+menu.addEventListener('click', function (){
+    navigation.classList.toggle('active')
+    menu.classList.toggle('active')
+})
+
+ function modeIcon(){
+    i[0].toggleAttribute('data' );
+    i[1].toggleAttribute( "data");
+}
